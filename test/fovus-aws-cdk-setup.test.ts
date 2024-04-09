1e13fbf5d6f56b57932864a0dc708cb964b31b5b
Drake <yuxuan.z.zhou@gmail.com>
2024-04-09 01:05:25 -0400

    Initial commit

---
+++ test/fovus-aws-cdk-setup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FovusAwsCdkSetup from '../lib/fovus-aws-cdk-setup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fovus-aws-cdk-setup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FovusAwsCdkSetup.FovusAwsCdkSetupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
